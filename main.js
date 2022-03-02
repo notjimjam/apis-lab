e694ffa090869d321945571699cc7d3d8d041f49
Jamie Smith <thejamiemsmith@gmail.com>
2022-03-02 16:08:48 -0700

    complete through making a request

--- main.js
+++ main.js
@@ -1,8 +1,3 @@
-// const { default: axios } = require("axios")
-
-// const { default: axios } = require("axios")
-
-// const axios = require("axios")
 
 console.log('hello world')
 
@@ -12,11 +7,18 @@ const residentsBtn = document.querySelector('#residents')
 // const baseURL = 'https://swapi.dev/api'
 
 function getResidents (event) {
-    // event.preventDefault()
+    event.preventDefault()
 
-    axios.get('https://swapi.dev/api')
+    axios.get('https://swapi.dev/api/planets/2')
         .then(res => {
-          console.log(res.data)  
+          for(let i = 0; i < res.data.residents.length; i++) {
+            axios.get(`${res.data.residents[i]}`)
+            .then(res => {
+                const header = document.createElement('h2')
+                header.textContent = res.data.name
+                document.querySelector('body').appendChild(header)
+            })
+          }
         })
         .catch(error => {
             console.log(error)
@@ -27,7 +29,3 @@ function getResidents (event) {
 
 residentsBtn.addEventListener('click', getResidents)
 
-axios.get('https://swapi.dev/api')
-    .then(res => {
-        console.log(res.data)
-    })
